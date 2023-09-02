d6cab3244250c90cf57f4119ea8662c8311c3f48
denvergeeks <jen@denvergeeks.com>
2023-09-02 11:47:39 -0600

    Update api-setup.js

--- javascripts/discourse/api-initializers/api-setup.js
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -2,47 +2,31 @@ import { apiInitializer } from "discourse/lib/api";
 import loadScript from "discourse/lib/load-script";
 import I18n from "I18n";
 
-async function applyQrcode(element) {
-  const qrcodes = element.querySelectorAll(".d-wrap[data-wrap=qrcode]");
-  if (!qrcodes.length) {
+async function applyHighlight(element) {
+  const highlights = element.querySelectorAll(".d-wrap[data-wrap=highlight]");
+  if (!highlights.length) {
     return;
   }
-
-  await loadScript(settings.theme_uploads_local.qrcode_js);
-
-  qrcodes.forEach((qrcode) => {
-    const qr = document.createElement("div");
-    qr.classList.add("qrcode");
-    const url = qrcode.getAttribute("data-url")
-      ? qrcode.getAttribute("data-url")
-      : window.location.href;
-    /*eslint no-new: 0 */
-    new window.QRCode(qr, url);
-    let node = document.createTextNode(url);
-
-    qrcode.appendChild(node);
-    qrcode.append(qr);
-  });
 }
 
 export default apiInitializer("0.11.1", (api) => {
   const { iconNode } = require("discourse-common/lib/icon-library");
-  iconNode("qrcode");
+  iconNode("paint-brush");
 
   const currentLocale = I18n.currentLocale();
   // I18n.translations[currentLocale].js.qrcode_button_title = I18n.t(themePrefix("composer_footnote_button_title"));
   // I18n.translations[currentLocale].js.composer.qrcode_button_text = I18n.t(themePrefix("composer_footnote_button_text"));
   I18n.translations[currentLocale].js.qrcode_button_title = "Insert QR Code";
   I18n.translations[currentLocale].js.composer.qrcode_button_text =
-    "Insert QR Code";
+    "Highlight Text";
 
   api.modifyClass("controller:composer", {
-    pluginId: "qrcode",
+    pluginId: "highlight",
 
     actions: {
-      qrcodeButton() {
+      highlightButton() {
         this.get("toolbarEvent").addText(
-          "\n" + `[wrap=qrcode url=]` + "\n[/wrap]\n"
+          "\n" + `[wrap=highlight]` + "\n[/wrap]\n"
         );
       },
     },
@@ -50,20 +34,20 @@ export default apiInitializer("0.11.1", (api) => {
 
   api.addToolbarPopupMenuOptionsCallback(() => {
     return {
-      icon: "qrcode",
-      label: "qrcode_button_title",
-      action: "qrcodeButton",
+      icon: "paint-brush",
+      label: "highlight_button_title",
+      action: "highlightButton",
     };
   });
 
   // add button rather than to the menu
   // api.onToolbarCreate((toolbar) => {
   //   toolbar.addButton({
-  //     id: "composer_qrcode",
+  //     id: "composer_highlight",
   //     group: "extras",
-  //     icon: "qrcode",
-  //     title: "qrcode_button_text",
-  //     perform: (e) => e.addText("\n" + `[wrap=qrcode url=]` + "\n[/wrap]\n"),
+  //     icon: "highlight",
+  //     title: "highlight_button_text",
+  //     perform: (e) => e.addText("\n" + `[wrap=highlight]` + "\n[/wrap]\n"),
   //   });
   // });
 
@@ -72,6 +56,6 @@ export default apiInitializer("0.11.1", (api) => {
       const id = helper ? `post_${helper.getModel().id}` : "composer";
       applyQrcode(elem, id);
     },
-    { id: "discourse-qrcode-theme-component" }
+    { id: "discourse-highlight-wrap-theme-component" }
   );
 });
