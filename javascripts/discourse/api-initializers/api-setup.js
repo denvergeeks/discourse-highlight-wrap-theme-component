15fffd64c76e9979194e33d23479a7b7a65ee4b0
denvergeeks <jen@denvergeeks.com>
2023-09-03 05:19:06 -0600

    Update api-setup.js

--- javascripts/discourse/api-initializers/api-setup.js
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -13,10 +13,10 @@ export default apiInitializer("0.11.1", (api) => {
   const { iconNode } = require("discourse-common/lib/icon-library");
   let icon = iconNode("highlighter");
   const currentLocale = I18n.currentLocale();
-  // I18n.translations[currentLocale].js.highlight_button_title = "Highlight Text";
-  // I18n.translations[currentLocale].js.composer.highlight_button_text = "Highlight Text";
-  I18n.translations[currentLocale].js.highlight_button_title = I18n.t(themePrefix("composer_highlight_button_title"));
-  I18n.translations[currentLocale].js.composer.highlight_button_text = I18n.t(themePrefix("composer_highlight_button_text"));
+  // I18n.translations[currentLocale].js.highlight_button_title = I18n.t(themePrefix("composer_highlight_button_title"));
+  // I18n.translations[currentLocale].js.composer.highlight_button_text = I18n.t(themePrefix("composer_highlight_button_text"));
+  I18n.translations[currentLocale].js.highlight_button_title = "Highlight Text";
+  I18n.translations[currentLocale].js.composer.highlight_button_text = "Highlight Text";
 
   api.modifyClass("controller:composer", {
     pluginId: "highlight",
